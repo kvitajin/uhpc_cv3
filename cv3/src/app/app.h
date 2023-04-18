c515824017c6e3f523d9b55faa1ad7b97cd7a461
Jindřich Kvita <jindrich.kvita@gnj.cz>
2023-04-18 16:39:02 +0200

    cleanup

--- cv3/src/app/app.h
+++
@@ -1,7 +0,0 @@
-#include <string>
-#include <sstream>
-#include <iostream>
-#include <timelibrary.h>
-
-inline std::string getEnvVar(const std::string &param, const std::string defaultValue);
-int main();
